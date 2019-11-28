4e779c8431c6849c8aeae8a4ff22f427b3ead5e0
Lucas de Carvalho Alves <lucasdecarvalho.ti@gmail.com>
2019-11-28 10:39:40 -0200

    Rocketseat module finished, introduction to react

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -2,15 +2,30 @@ import React, { Component } from 'react';
 import TechItem from './TechItem';
 
 class TechList extends Component {
-
   state = {
     newTech: '',
-    techs: [
-      'NodeJS',
-      'ReactJS',
-      'React Native'
-    ]
+    techs: []
   };
+
+  // Executado assim que o componente aparece na tela
+  componentDidMount() {
+    const techs = localStorage.getItem('techs');
+    if(techs) {
+      this.setState({ techs: JSON.parse(techs) })
+    }
+  }
+
+  // Executado sempre que ouver alterações nas props/estado
+  componentDidUpdate(_, prevState) {
+    if(prevState.techs !== this.state.techs) {
+      localStorage.setItem('techs', JSON.stringify(this.state.techs));
+    }
+  }
+
+  // Executado quando o component deixa de existir
+  // componentWillUnmount() {
+
+  // }
 
   handleInputChange = e => {
     this.setState({ newTech: e.target.value });
